bd36217814280e93733858768fa102f7772a74e3
webdev.sp <webdev.sp@amtexsystems.com>
2023-07-14 17:49:56 +0530

    added no orders

--- src/pages/seller/SellerOrders.jsx
+++ src/pages/seller/SellerOrders.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react"
 import {getSellerOrderInfo} from "../../apis/orders/getSellerOrderInfo"
 import Table from "../../components/Table"
-import {Container} from "@mui/material";
+import {Container, Typography} from "@mui/material";
 
 const SellerOrders=()=>{
     const [orderedItems,setOrderedItems] = useState()
@@ -24,7 +24,8 @@ const SellerOrders=()=>{
           maxWidth="lg"
           sx={{ padding: "20px 0px", minHeight: "85vh", textAlign: "center" }}
         >
-            <Table orderedItems={orderedItems} handleRefetch={handleRefetch}/>
+            {orderedItems.length > 0 &&<Table orderedItems={orderedItems} handleRefetch={handleRefetch}/>}
+            {orderedItems.length === 0 && <Typography>No orders</Typography>}
         </Container>
     )
 }
